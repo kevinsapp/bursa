725d6414b3bbe420859e7a12d7f441132eefd792
Kevin Sapp <sapp.kevin@gmail.com>
2019-05-01 12:55:27 -0400

    Don't hard code title tag in head

--- functions.php
+++ functions.php
@@ -16,6 +16,14 @@
   */
 function bursa_setup() {
   /*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+  /*
    * Enable support for Post Thumbnails on posts and pages.
    *
    * @link https://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
--- header.php
+++ header.php
@@ -19,7 +19,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="<?php bloginfo( 'description' ); ?>" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-  <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" media="all">
   <?php wp_head(); ?>
