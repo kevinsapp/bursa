f10ca32378bd2ee96f380e75ec0ed105624303f2
Kevin Sapp <sapp.kevin@gmail.com>
2019-05-01 15:57:02 -0400

    Enqueue 'comment-reply' script

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
   <meta name="description" content="<?php bloginfo( 'description' ); ?>" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
+  <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
   <?php wp_head(); ?>
 </head>
 
