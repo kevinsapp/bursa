1aace78a5b2f9602b890d4a780d899102ca44469
Kevin Sapp <sapp.kevin@gmail.com>
2019-05-04 06:38:25 -0400

    Set global content width
    Required by Theme Check for wordpress.org compatible themes.

--- functions.php
+++ functions.php
@@ -141,6 +141,21 @@ function bursa_widgets_init() {
 add_action( 'widgets_init', 'bursa_widgets_init' );
 
 /**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width Content width.
+ */
+function bursa_content_width() {
+	// This variable is intended to be overruled from themes.
+	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	$GLOBALS['content_width'] = apply_filters( 'bursa_content_width', 768 );
+}
+add_action( 'after_setup_theme', 'bursa_content_width', 0 );
+
+/**
  * Enqueue styles and scripts.
  *
  * @since 1.0.0
