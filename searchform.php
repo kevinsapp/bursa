79d273c6ce96d3c9481d9933df9d81a1f67be7e9
Kevin Sapp <sapp.kevin@gmail.com>
2019-05-06 19:05:09 -0400

    Update searchform.php
    Default to more generic text

--- searchform.php
+++ searchform.php
@@ -5,10 +5,10 @@
 ?>
 <form role="search" method="get" class="search-form my-4 mt-md-0" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <div class="input-group">
-    <input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search Outsiders Republic&hellip;', 'placeholder', 'bursa' ); ?>" value="" name="s" id="s" aria-label="<?php echo _x( 'Search for:', 'label', 'bursa' ); ?>" aria-describedby="search-submit">
+    <input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search&hellip;', 'placeholder', 'bursa' ); ?>" value="" name="s" id="s" aria-label="<?php echo _x( 'Search for:', 'label', 'bursa' ); ?>" aria-describedby="search-submit">
     <div class="input-group-append">
       <button class="btn btn-outline-secondary" type="button" id="search-submit">
-        Search
+        Go
       </button>
     </div>
   </div>
